37aff3804c0c3c4e500fd8a4d33f363627f50feb
HuanMeng-official <3099044113@qq.com>
2026-02-07 22:30:19 +0800

    refactor: 调整Windows下默认窗口大小

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,7 +26,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(400, 700);
+  Win32Window::Size size(1080, 720);
   if (!window.Create(L"LumenFlow", origin, size)) {
     return EXIT_FAILURE;
   }
